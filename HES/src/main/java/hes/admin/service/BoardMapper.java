172a3cbe3b3f2a00b1323e9df58a441641888113
82109 <82109@DESKTOP-T6EG00J>
2021-01-13 15:00:52 +0900

    board 그냥 가져옴 자주묻는질문이나 공지사항이나 뭐 하나 하죠

---
+++ HES/src/main/java/hes/admin/service/BoardMapper.java
@@ -0,0 +1,57 @@
+package hes.admin.service;
+
+import java.util.*;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import hes.admin.model.BoardDTO;
+
+
+@Service
+public class BoardMapper {
+	@Autowired
+	private SqlSession sqlSession;
+
+	public List<BoardDTO> listBoard(int start, int end) {
+		Map<String, Integer> map = new Hashtable<>();
+		map.put("start", start);
+		map.put("end", end);
+		List<BoardDTO> list = sqlSession.selectList("listBoard", map);
+		return list;
+	}
+
+	public int getCount() {
+			Integer count = sqlSession.selectOne("getCount");
+			return count;
+	}
+
+	public int insertBoard(BoardDTO dto) {
+		String sql = null;
+		if (dto.getNum() == 0) {
+			sql = "update board set re_step = re_step + 1";
+		} else {
+			sql = "update board set re_step = re_step + 1 where re_step > " + dto.getRe_step();
+			dto.setRe_step(dto.getRe_step() + 1);
+			dto.setRe_level(dto.getRe_level() + 1);
+		}
+		Map<String, String> map = new Hashtable<>();
+		map.put("sql", sql);
+		sqlSession.update("plusRe_step", map);
+		int res = sqlSession.insert("insertBoard", dto);
+		sqlSession.commit();
+		return res;
+	}
+	
+	public BoardDTO getBoard(int num, String mode) {
+		if (mode.equals("content")) {
+			sqlSession.update("plusReadCount",num);
+		}
+		//sqlSession.commit();
+		BoardDTO dto = sqlSession.selectOne("getBoard",num);
+		return dto;
+	}
+	
+	
+}
